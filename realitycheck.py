96b3f13e5497ce87081b74dffe7ab56497ae45f5
paul <h4xx@me.com>
2021-09-05 21:18:28 +0200

    Comments and cleanup

--- realitycheck.py
+++ realitycheck.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
 
+# Either loop over all IPs or loop only over a certain set
+# defined by the label argument passed from commandline.
+#
+# The loop runs a http request and the exit code is used to
+# decide if the host is reachable or not.
+#
+# The list if IPs is hardcoded in dict style with each matching
+# label as a value to the IP.
+
+import sys
 import requests
 
+# Hardcoded dictionary of IPs with matching labels
 ip_dict={
-"143.198.186.125":["DIG","WEB","COS"],
-"157.230.30.0":["DIG","GTW"]
+"143.198.186.125":["dig","web","centos"],
+"157.230.30.0":["dig","gateway"]
 }
 
+# Sends the http request, returns the statuc code
 def ring(door):
     r = requests.get("http://" + door)
     return(r.status_code)
 
-for ip,types in ip_dict.items():
-    if any(type == "DIG" for type in types):
+# Main
+if __name__ == "__main__":
+    for ip,types in ip_dict.items():
         print("Send request to " + ip)
         if ring(ip) != 200:
             print("%s is not reachable" % ip)
+        else:
+            print("%s is reachable" % ip)
