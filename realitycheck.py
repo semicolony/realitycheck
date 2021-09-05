96886751cc74cc0bb65c307cb06731e1d8954acf
paul <h4xx@me.com>
2021-09-05 18:06:03 +0200

    First basic check function against a websocket.

--- realitycheck.py
+++ realitycheck.py
@@ -1,1 +1,18 @@
-print("hello world")
+#!/usr/bin/python3
+
+import requests
+
+ip_dict={
+"143.198.186.125":["DIG","WEB","COS"],
+"157.230.30.0":["DIG","GTW"]
+}
+
+def ring(door):
+    r = requests.get("http://" + door)
+    return(r.status_code)
+
+for ip,types in ip_dict.items():
+    if any(type == "DIG" for type in types):
+        print("Send request to " + ip)
+        if ring(ip) != 200:
+            print("%s is not reachable" % ip)
